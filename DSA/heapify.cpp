102d65b89cce193e3ca25e5327dc6a1696896574
Gautam-Anand <gautamanand931@gmail.com>
2025-03-10 08:32:34 +0000

    array made in heapify

---
+++ DSA/heapify.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {45,22,67,99,12,34,89,77,11,6};
+    for(int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
